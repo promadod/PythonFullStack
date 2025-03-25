f60440dda84569b9e880bf0a9c6b43fc3c15183a
Juan <juanvictor1511@hotmail.com>
2025-03-24 21:41:40 -0300

    atualizado11

--- exercicios_aula11.js
+++ exercicios_aula11.js
@@ -4,53 +4,55 @@
 //Adicione métodos para reservarQuarto() e cancelarReserva().
 //Crie uma classe Cliente com nome, CPF e historicoReservas.
 //Implemente a interação entre elas.
-
-class reservasHotel{
-    constructor(nome, cidade, quartosDisponiveis){
+class ReservasHotel {
+    constructor(nome, cidade, quartosDisponiveis) {
         this.nome = nome;
         this.cidade = cidade;
         this.quartosDisponiveis = quartosDisponiveis;
     }
-    
 }
 
-class SistemaReservas{
-    reservarQuarto(quarto){
-        if(quarto.quartosDisponiveis > 0){
+class SistemaReservas {
+    constructor() {
+        this.hotel = []; // Array de hotéis
+        this.clientes = []; // Lista de clientes
+    }
+
+    reservarQuarto(quarto) {
+        if (quarto.quartosDisponiveis > 0) {
             quarto.quartosDisponiveis--;
             return true;
-        }else{
+        } else {
             return false;
         }
-}
-    constructor(){
-        this.hotel = [];
-        this.clientes = [];
     }
-}       
-class Cliente{
-    constructor(nome, cpf, historicoReservas){
+}
+
+class Cliente {
+    constructor(nome, cpf, historicoReservas) {
         this.nome = nome;
         this.cpf = cpf;
         this.historicoReservas = historicoReservas;
     }
 }
 
+const sistema = new SistemaReservas();
 
-    const sistema = new SistemaReservas();
-    
-    cliente1 = new Cliente("Yuri Silva", "123.456.789-00", []);
-    cliente2 = new Cliente("Katelyn Souza", "987.654.321-00", []);
-    
-    sistema.clientes.push(cliente1);
-    sistema.clientes.push(cliente2);
-    
-    quarto1 = new reservasHotel("Hilton", "Recreio dos Bandeirantes", 10);
-    quarto2 = new reservasHotel("Copacabana Pallace", "Copacabana", 5);
-    
-    sistema.Hilton.push(quarto1);
-    sistema.Copacabanapallace.push(quarto2);
-    
-    console.log(sistema.reservarQuarto(quarto1));
-    console.log(sistema.reservarQuarto(quarto2));
-    console.log(sistema.reservarQuarto(quarto2));
+const cliente1 = new Cliente("Yuri Silva", "123.456.789-00", []);
+const cliente2 = new Cliente("Katelyn Souza", "987.654.321-00", []);
+
+sistema.clientes.push(cliente1);
+sistema.clientes.push(cliente2);
+
+const quarto1 = new ReservasHotel("Hilton", "Recreio dos Bandeirantes", 10);
+const quarto2 = new ReservasHotel("Copacabana Pallace", "Copacabana", 5);
+
+sistema.hotel.push(quarto1);
+sistema.hotel.push(quarto2);
+
+if (sistema.reservarQuarto(quarto1)) {
+    cliente1.historicoReservas.push(quarto1);
+    console.log("Quarto reservado com sucesso!");
+} else {
+    console.log("Quarto indisponível.");
+}
